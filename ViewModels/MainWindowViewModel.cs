fe46c308ef3eb1be3c39eca680e9b01b6b749336
Andrew Lee <alee14498@protonmail.com>
2021-02-01 01:05:42 -0500

    Added project files

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alee_Audio_Player.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
