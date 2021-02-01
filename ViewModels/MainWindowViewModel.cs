5a5fb453305ae43f981b9061fc9444068e493fdc
Andrew Lee <alee14498@protonmail.com>
2021-02-01 16:29:53 -0500

    It can now play audio!

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Gst;
 
 namespace AleeAudioPlayer.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
         public string Greeting => "Welcome to Alee Audio Player!";
+
+        public void PlayFunction(string[] args)
+        {
+            Application.Init(ref args);
+            // Build the pipeline
+            var pipeline = Parse.Launch("playbin uri=file:///home/andrew/Music/4616-werq-by-kevin-macleod.mp3");
+
+            // Start playing
+            pipeline.SetState(State.Playing);
+
+            // Wait until error or EOS
+            var bus = pipeline.Bus;
+            var msg = bus.TimedPopFiltered (Constants.CLOCK_TIME_NONE, MessageType.Eos | MessageType.Error);
+
+            // Free resources
+            //pipeline.SetState (State.Null);
+        }
     }
 }
