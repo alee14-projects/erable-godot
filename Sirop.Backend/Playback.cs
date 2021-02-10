834d2891edaf996da59908142d16120ac7c1f32a
Andrew Lee <alee14498@protonmail.com>
2021-02-10 15:37:37 -0500

    Fixed license

--- Sirop.Backend/Playback.cs
+++ Sirop.Backend/Playback.cs
@@ -1,12 +1,12 @@
 /**********************************************************************
  *
- *   Sirop Backend: Erable Audio Player's Backend
- *   Copyright (C) 2021 Alee Productions
+ *    Sirop Backend: Erable Audio Player's Backend
+ *    Copyright (C) 2021 Alee Productions
  *
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
  *
  *    This program is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
