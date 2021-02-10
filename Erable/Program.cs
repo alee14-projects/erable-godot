7940cebbb91ab947ee9306e01371669dab60b09a
Andrew Lee <alee14498@protonmail.com>
2021-02-10 15:30:37 -0500

    Licensed the files

--- Erable/Program.cs
+++ Erable/Program.cs
@@ -1,4 +1,23 @@
-﻿using System;
+﻿/**********************************************************************
+ *
+ *   Erable: Audio Player
+ *   Copyright (C) 2021 Alee Productions
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ ************************************************************************/
+using System;
 using Avalonia;
 using Avalonia.ReactiveUI;
 
@@ -19,9 +38,7 @@ namespace Erable
             Console.WriteLine("Running on .NET " + Environment.Version + ", and " + Environment.OSVersion);
             Console.WriteLine("Opening window...");
             #endif
-            
-            DiscordRpc.Initialize();
-            
+
             BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
 
         }
