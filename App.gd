6cb9ecfd6f6c863cd873a1b5e77972d542dfb1bc
Andrew Lee <alee14498@protonmail.com>
2021-11-24 23:50:05 -0500

    Added a file dialog; Hopefully it can play audio soon

---
+++ App.gd
@@ -0,0 +1,27 @@
+extends Control
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var audio;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("Erable is ready")
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _on_PlayButton_pressed():
+	pass # Replace with function body.
+
+func _on_BrowseButton_pressed():
+	$FileDialog.popup()
+
+func _on_FileDialog_file_selected(path):
+	print(path)
+	audio = path
+
