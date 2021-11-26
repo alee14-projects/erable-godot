7f1cdc4663800fa9bdfddf888164826819d97e6e
Andrew Lee <alee14498@protonmail.com>
2021-11-26 09:22:33 -0500

    Almost got music playing

--- App.gd
+++ App.gd
@@ -4,24 +4,28 @@ extends Control
 # var a = 2
 # var b = "text"
 
-var audio;
+var audioURL
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("Erable is ready")
-	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 func _on_PlayButton_pressed():
-	pass # Replace with function body.
+	print("Now playing music")
+	$AudioStreamPlayer.stream = load(audioURL)
+	$AudioStreamPlayer.play()
 
 func _on_BrowseButton_pressed():
-	$FileDialog.popup()
+	print("Opening file dialog")
+	$FileDialog.popup_centered()
 
 func _on_FileDialog_file_selected(path):
 	print(path)
-	audio = path
+	audioURL = "file://" + path
 
+func _on_AudioStreamPlayer_finished():
+	print("Music has finished playing")
