201a0de90faabe68f1eb701b67af30a4d11fa32b
Andrew Lee <alee14498@protonmail.com>
2023-08-07 13:37:19 -0400

    Public archive

--- App.gd
+++ App.gd
@@ -5,6 +5,7 @@ extends Control
 # var b = "text"
 
 var audioURL
+var audioFile = File.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,17 +16,27 @@ func _ready():
 #	pass
 
 func _on_PlayButton_pressed():
-	print("Now playing music")
-	$AudioStreamPlayer.stream = load(audioURL)
-	$AudioStreamPlayer.play()
+	if !audioURL == null:
+		$AudioStreamPlayer.play()
+		print("Now playing " + audioURL)
+	else:
+		print("No music detected")
+	
+func _on_StopButton_pressed():
+	pass # Replace with function body.
+
 
 func _on_BrowseButton_pressed():
 	print("Opening file dialog")
+#	$FileDialog.filters
 	$FileDialog.popup_centered()
 
 func _on_FileDialog_file_selected(path):
-	print(path)
-	audioURL = "file://" + path
+	audioFile.open(path, File.READ).get_as_text().close()
+	$AudioStreamPlayer.stream = load(audioFile)
+	
 
 func _on_AudioStreamPlayer_finished():
 	print("Music has finished playing")
+
+
